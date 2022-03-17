6a86136fcbe11d3c1d72af5225d6c7ca65586213
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-03-16 23:40:57 -0300

    feat: Adicionado Titulo, Fonts e criado os arquivos de css. Criado arquivo .env. Criado estrutura básica <App /> no index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDom from "react-dom";
+
+function App(){
+    return (
+        <>
+            <h1>Teste</h1>
+        </>
+    )
+}
+
+ReactDom.render(<App />,document.querySelector(".root"));
